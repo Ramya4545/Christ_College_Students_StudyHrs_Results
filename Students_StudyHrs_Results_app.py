dec2166c79397d7bede797a7d16f0bfe1bada91b
Ramya4545 <ramyapaila732@gmail.com>
2026-09-17 09:24:36 +0530

    Update Students_StudyHrs_Results_app.py

--- Students_StudyHrs_Results_app.py
+++ Students_StudyHrs_Results_app.py
@@ -8,7 +8,7 @@ st.title("Student Pass/Fail based on Study Hours")
 hours = st.number_input("Enter Study Hours", min_value = 0.0, max_value = 15.0, value = 5.0)
 
 if st.button("Predict"):
-  prediction = model.predict([hours])
+  prediction = model.predict([[hours]])
 
   if prediction[0] == 1:
     st.success("PASS")
